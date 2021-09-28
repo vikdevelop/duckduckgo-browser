e81284bd85d5566569ab0ef255476bad7303e2c5
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2021-09-28 15:21:13 +0200

    Build script for flatpak

---
+++ build-flatpak.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+# build script for flatpak.
+
+cmake -B build
+cd build
+make
+install -Dm755 -t /app/bin build/duckduckgobrowser
+install -Dm755 -t /app/share/applications data/ithub.vikdevelop.duckduckgobrowser.desktop
+install -Dm644 -t /app/share/metainfo data/com.github.vikdevelop.duckduckgobrowser.metainfo.xml
+install -Dm755 -t /app/share/icons/hicolor/128x128/apps data/icons/hicolor/128x128/apps/com.github.vikdevelop.duckduckgobrowser.png
