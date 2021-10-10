0e99f041775bb743d5451ffc35722ba970592afc
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2021-10-10 15:35:35 +0200

    Update build-flatpak.sh

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -5,6 +5,6 @@ cmake -B build
 cd build
 make
 install -Dm755 -t /app/bin build/duckduckgobrowser
-install -Dm755 -t /app/share/applications data/ithub.vikdevelop.duckduckgobrowser.desktop
+install -Dm755 -t /app/share/applications data/github.vikdevelop.duckduckgobrowser.desktop
 install -Dm644 -t /app/share/metainfo data/com.github.vikdevelop.duckduckgobrowser.metainfo.xml
 install -Dm755 -t /app/share/icons/hicolor/128x128/apps data/icons/hicolor/128x128/apps/com.github.vikdevelop.duckduckgobrowser.png
